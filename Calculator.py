ca907823432b9e2a3b8073086984806ba1b12bd1
Harshith <harshithdeva1@gmail>
2024-09-07 12:44:23 +0530

    Reformated App

--- Calculator.py
+++ Calculator.py
@@ -5,70 +5,74 @@ import csv
 import os
 from local_config import PATH
 
-def split_rent_calculator(month_rent_list,rent_list):
+
+def split_rent_calculator(month_rent_list, rent_list):
     previous_month_rent = month_rent_list[-2]
-    split_rent = rent_list[-1]-previous_month_rent
+    split_rent = rent_list[-1] - previous_month_rent
     return split_rent
 
-def get_last_day(month,year):
-    last_day = calendar.monthrange(year,month)
+
+def get_last_day(month, year):
+    last_day = calendar.monthrange(year, month)
     last_day = list(last_day)
     return last_day[1]
 
 
-def total_rent_for_month(rent,last_day,start_date):
+def total_rent_for_month(rent, last_day, start_date):
     last_day = int(last_day)
     due_day = start_date.strftime("%d")
     due_day = int(due_day)
-    month_rent = (rent/last_day)*(last_day-due_day)
-    return int(round(month_rent))
+    month_rent = (rent / last_day) * (last_day - due_day)
+    return round(month_rent)
 
-    
-def rent_hike_date(start_date,end_dates):
+
+def rent_hike_date(start_date, end_dates):
     hike_dates = list()
     while start_date < end_dates:
-        hike_dates.append(start_date+relativedelta(years=1))
-        start_date = start_date+relativedelta(years=1)
+        hike_dates.append(start_date + relativedelta(years=1))
+        start_date = start_date + relativedelta(years=1)
     return hike_dates
 
-def calculate_first_month_rent(start_date,rent):
-    last_day = get_last_day(start_date.month,start_date.year)
-    month_rent = total_rent_for_month(rent,last_day,start_date)
-    return month_rent,last_day
 
-def calculate_rent(start_date,end_dates,rent,rate):
+def calculate_first_month_rent(start_date, rent):
+    last_day = get_last_day(start_date.month, start_date.year)
+    month_rent = total_rent_for_month(rent, last_day, start_date)
+    return month_rent, last_day
+
+
+def calculate_rent(start_date, end_dates, rent, rate):
     current_date = start_date
     month_rent_list = []
     rent_list = []
     data_list = []
     rent_list.append(rent)
-    first_month_rent,f_last_day = calculate_first_month_rent(start_date,rent)
+    first_month_rent, f_last_day = calculate_first_month_rent(start_date, rent)
     month_rent_list.append(first_month_rent)
-    data_list.append([start_date,"",f_last_day,first_month_rent,"",""])
-    #print(f"Due Date: {start_date}")
-    #print(f"Rent: {rent}")
-    #print(f"Last Day of Month: {f_last_day}")
-    #print(f"Month End Rent: {first_month_rent}")
+    data_list.append([start_date, "", f_last_day, first_month_rent, "", ""])
+    # print(f"Due Date: {start_date}")
+    # print(f"Rent: {rent}")
+    # print(f"Last Day of Month: {f_last_day}")
+    # print(f"Month End Rent: {first_month_rent}")
     hike_dates = rent_hike_date(start_date, end_dates)
 
     while current_date < end_dates:
         row_list = []
-        current_date = current_date+ relativedelta(months=1)
+        current_date = current_date + relativedelta(months=1)
         if current_date in hike_dates:
-            rent = rent+(rent*(rate/100))
-            rent = int(round(rent))
+            rent = rent + (rent * (rate / 100))
+            rent = round(rent)
         rent_list.append(rent)
-        last_day = get_last_day(current_date.month,current_date.year)
-        month_rent = total_rent_for_month(rent,last_day,start_date)
+        last_day = get_last_day(current_date.month, current_date.year)
+        month_rent = total_rent_for_month(rent, last_day, start_date)
         month_rent_list.append(month_rent)
-        split_rent = split_rent_calculator(month_rent_list,rent_list)
+        split_rent = split_rent_calculator(month_rent_list, rent_list)
         total_rent = split_rent + month_rent
-        #print(f"Due Date: {current_date}")
-        #print(f"Rent: {rent}")
-        #print(f"Last Day of Month: {last_day}")
-        #print(f"Month End Rent: {month_rent}")
-        #print(f"Due Day Rent : {split_rent}")
-        #print(f"Total Rent: {total_rent}")
+        # print(f"Due Date: {current_date}")
+        # print(f"Rent: {rent}")
+        # print(f"Last Day of Month: {last_day}")
+        # print(f"Month End Rent: {month_rent}")
+        # print(f"Due Day Rent : {split_rent}")
+        # print(f"Total Rent: {total_rent}")
         row_list.append(current_date)
         row_list.append(rent)
         row_list.append(last_day)
@@ -76,40 +80,47 @@ def calculate_rent(start_date,end_dates,rent,rate):
         row_list.append(split_rent)
         row_list.append(total_rent)
         data_list.append(row_list)
-    
-    header_list = ["Due Date","Rent", "Last Day of Month", "Month End Rent", "Due Day Rent", "Total Rent"]
-    csv_writer(header_list=header_list, data_list= data_list)
 
-def csv_writer(header_list,data_list):
-    
+    header_list = [
+        "Due Date",
+        "Rent",
+        "Last Day of Month",
+        "Month End Rent",
+        "Due Day Rent",
+        "Total Rent",
+    ]
+    csv_writer(header_list=header_list, data_list=data_list)
+
+
+def csv_writer(header_list, data_list):
+
     time = datetime.now()
     time = time.strftime("%d-%m-%Y")
     filename = f"data_{time}.csv"
     file_path = os.path.join(PATH, filename)
-    with open(file_path, 'w') as csvfile:
+    with open(file_path, "w") as csvfile:
         # creating a csv writer object
         csvwriter = csv.writer(csvfile)
         # writing the fields
         csvwriter.writerow(header_list)
         # writing the data rows
         csvwriter.writerows(data_list)
-              
 
 
-def main ():
-    start_date = input('What is the start date?\n')
-    end_date = input('What is the end date?\n')
-    rent_pm = input('What is the rent amount\n')
+def main():
+    start_date = input("What is the start date?\n")
+    end_date = input("What is the end date?\n")
+    rent_pm = input("What is the rent amount\n")
     rent_pm = int(rent_pm)
-    rate_increase = input('What is the Rate hike %\n')
+    rate_increase = input("What is the Rate hike %\n")
     rate_increase = int(rate_increase)
     try:
-        start_date = datetime.strptime(start_date,"%d-%m-%Y")
-        end_date = datetime.strptime(end_date,"%d-%m-%Y")
+        start_date = datetime.strptime(start_date, "%d-%m-%Y")
+        end_date = datetime.strptime(end_date, "%d-%m-%Y")
     except:
         print("Date Entered in the Incorrect Format")
-    
-    calculate_rent(start_date,end_date,rent_pm,rate_increase)
+
+    calculate_rent(start_date, end_date, rent_pm, rate_increase)
 
 
 main()
---
+++ app/routes.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, flash, url_for
+from app import app
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+@app.route("/")
+@app.route("/index", methods=["POST"])
+def index():
+    return render_template("input_page.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        try:
+            rent_pm = request.form["rent_pm"]
+            rent_pm = int(rent_pm)
+            rate_increase = request.form["rate_increase"]
+            rate_increase = int(rate_increase)
+            start_date = request.form["start_date"]
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = request.form["end_date"]
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+            if start_date > end_date:
+                flash("Starting Date cannot be greater than End Date")
+                return redirect(url_for("index"))
+        except:
+            flash("Something Went Wrong")
+            return redirect(url_for("index"))
+
+    else:
+        flash("Success")
+        return redirect(url_for("index"))
+    print(rent_pm)
+    print(rate_increase)
+    print(start_date)
+    print(end_date)
+    flash("Success")
+    return redirect(url_for("index"))
